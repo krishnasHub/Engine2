10e31e73c7a241082cb128f715942b05e7e602d2
vkrishnajayanthi <vkrishnajayanthi>
2016-11-12 18:57:25 -0500

    Initial Build

---
+++ Engine2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+
+namespace Engine2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new GameWindow(800, 600);
+
+            g.Run();
+        }
+    }
+}
